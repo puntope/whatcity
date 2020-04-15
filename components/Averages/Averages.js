000739551c6e13d7169edc03f65cdcca4fd31cde
miguel <miguel@buidinamsterdam.com>
2020-04-15 23:50:46 +0200

    Add Material UI Table instead of mine in flex

--- components/Averages/Averages.js
+++ components/Averages/Averages.js
@@ -1,9 +1,15 @@
 import s from './Averages.scss'
 import Termometer from "../Termometer";
 import React from "react";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Hero from "../Hero";
+import Paper from "@material-ui/core/Paper";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Table from "@material-ui/core/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableBody from "@material-ui/core/TableBody";
+import TableHead from "@material-ui/core/TableHead";
 
 const months = ['Jan', 'Feb', 'March', 'April', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 const Averages = ({cities}) => {
@@ -13,32 +19,35 @@ const Averages = ({cities}) => {
                 title="Average temperatures"
                 body="If click on the cards was not enough, here is the average temperature per month."
             />
-            <Container className={s.tableWrap} maxWidth="md" component="section">
-                <div className={s.table}>
-
-                    <div className={s.tr}>
-                        <div className={s.th}><Typography variant="subtitle1">City</Typography></div>
-                        {months.map(month => (
-                            <div key={month} className={s.th}><Typography variant="subtitle1">{month}</Typography></div>
-                        ))}
-                    </div>
-                    {
-                        cities.map(city => (
-                            <div key={city.code} className={s.tr}>
-                                <div className={s.td}>
-                                    <Typography variant="subtitle1">{city.name}</Typography>
-                                </div>
-                                {
-                                    city.weatherAvg.map((temperature, index) => (
-                                        <div key={index} className={s.td}>
-                                            <Termometer temperature={temperature} variant="subtitle1"/>
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                        ))
-                    }
-                </div>
+            <Container className={s.tableWrap} maxWidth="lg" component="section">
+                <TableContainer component={Paper}>
+                    <Table className={s.table} aria-label="simple table">
+                        <TableHead className={s.th}>
+                            <TableRow>
+                                <TableCell>City</TableCell>
+                                {months.map(month => (
+                                    <TableCell key={month}>{month}</TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {cities.map((city) => (
+                                <TableRow key={city.code}>
+                                    <TableCell component="th" scope="row">
+                                        {city.name}
+                                    </TableCell>
+                                    {
+                                        city.weatherAvg.map((temperature, index) => (
+                                            <TableCell key={index} align="center">
+                                                <Termometer temperature={temperature} variant="subtitle1"/>
+                                            </TableCell>
+                                        ))
+                                    }
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </Container>
         </div>
     )
